e3d9a0337e58a4064ff14f192f4a3c57a2498d60
victorprens <vprens12@gmail.com>
2021-10-05 22:39:07 -0500

    Creates the Item model.

--- groceryapp/models.py
+++ groceryapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    price = models.float_field()
+    price = models.FloatField()
     purchased = models.BooleanField(default=False)
-    purchased_at = models.datetime_field(auto_now_add=True)
-    updated_at = models.datetime_field(auto_now=True)
+    purchased_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
